ad35966a882a96f9029d77e71280210c631ebebc
Nikita <nikitapohod@icloud.com>
2022-09-16 14:56:19 +0300

    Program file added

---
+++ Program_GB/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] Filter(string[] array, int maxCountOfChars)
+{
+    string[] resArray = {};
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxCountOfChars)
+        {
+            Array.Resize(ref resArray, resArray.Length + 1);
+            resArray[resArray.Length-1] = array[i];
+        }
+    }
+    return resArray;
+}
+
+// Example
+string[] array = {"qwerty", "qwe", "fghh", "43"};
+var result = Filter(array, 3);
+foreach (var item in result) Console.Write($"{item} ");
+
+// Output:
+// qwe 43
